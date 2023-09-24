7f1b28a4e39c60d66cd5cd62899223704dfd8ca3
Eugenie Cheng <>
2023-09-25 00:34:04 +0800

    init

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,37 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { type Metadata } from "next";
 
-const inter = Inter({ subsets: ['latin'] })
+import { Providers } from "@/app/providers";
+import { Layout } from "@/components/Layout";
+
+import "@/styles/tailwind.css";
 
 export const metadata: Metadata = {
-  title: 'Eugenie Cheng',
-  description: 'A profile page',
-}
+  title: {
+    template: "%s - Eugenie Cheng",
+    default: "Eugenie Cheng's Page",
+  },
+  description: "Welcome to my page",
+  alternates: {
+    types: {
+      "application/rss+xml": `${process.env.NEXT_PUBLIC_SITE_URL}/feed.xml`,
+    },
+  },
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className="h-full  w-full antialiased" suppressHydrationWarning>
+      <body className=" h-full w-full dark:bg-zinc-800">
+        <Providers>
+          <div className="flex w-full">
+            <Layout>{children}</Layout>
+          </div>
+        </Providers>
+      </body>
     </html>
-  )
+  );
 }
