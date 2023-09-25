7253faec579c472057c414099bccaa32604f5022
Eugenie Cheng <eugenie.630.cheng@gmail.com>
2023-09-25 23:24:25 +0800

    update content

--- src/app/uses/page.tsx
+++ src/app/uses/page.tsx
@@ -42,8 +42,8 @@ export const metadata = {
 export default function Uses() {
   return (
     <SimpleLayout
-      title="Software I use, gadgets I love, and other things I recommend."
-      intro="I get asked a lot about the things I use to build software, stay productive, or buy to fool myself into thinking I’m being productive when I’m really just procrastinating. Here’s a big list of all of my favorite stuff."
+      title="Software, Gadgets, and somethings I recommend."
+      intro="Here’s a list of all of my favorite stuff for development."
     >
       <div className="space-y-20">
         <ToolsSection title="Workstation">
